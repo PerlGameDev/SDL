71c323f9f6f8b9235d34476910dd7648da48a9d1
Leon Brocard <acme@astray.com>
2009-10-16 14:23:29 +0100

    Add missing PixelFormat.pm

---
+++ lib/SDL/PixelFormat.pm
@@ -0,0 +1,31 @@
+package SDL::PixelFormat;
+use strict;
+use warnings;
+require Exporter;
+require DynaLoader;
+our @ISA = qw(Exporter DynaLoader);
+bootstrap SDL::PixelFormat;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+SDL::PixelFormat - Stores surface format information
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+An C<SDL::PixelFormat > stores surface format information
+
+=head1 METHODS
+
+=head1 SEE ALSO
+
+L<SDL::Surface>
+
+=cut
