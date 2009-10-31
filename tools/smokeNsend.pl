220633dbf9bddd27b9b61f4651a79d754716e010
Kartik Thakore <thakore.kartik@gmail.com>
2009-10-31 16:31:39 -0400

    [Smolder Tools] Gets platform and architecture automatically. Also can give git branch as paramater to update before test. prove merges stdout with stderr

--- tools/smokeNsend.pl
+++ tools/smokeNsend.pl
@@ -1,5 +1,6 @@
 #!perl
 #
-die "Usage: username password architecture [extra options for smolder_smoke_signal]. \n Found @ARGV args" if $#ARGV < 2;
-system split ' ', 'prove -l -b --archive sdl.tar.gz';
-system split ' ', "tools/smolder_smoke_signal smolder_smoke_signal --server sdlperl.ath.cx:8080  --username $ARGV[0] --password $ARGV[1] --file sdl.tar.gz --project SDL --architecture $ARGV[2] $ARGV[3]";
+die "Usage: username password [git-branch] [extra options for smolder_smoke_signal]. \n Found @ARGV args" if $#ARGV < 1;
+system split ' ', "git pull origin $ARGV[2]" if $ARGV[2];
+system split ' ', 'prove -l -b --merge --archive sdl.tar.gz';
+system split ' ', "tools/smolder_smoke_signal smolder_smoke_signal --server sdlperl.ath.cx:8080  --username $ARGV[0] --password $ARGV[1] --file sdl.tar.gz --project SDL --architecture $] --platform $^O $ARGV[3]";
