062699fa850a73730e1c410c3beb6e4c98f3456f
Kartik Thakore <thakore.kartik@gmail.com>
2009-10-31 16:44:49 -0400

    [Smoke tool] Builds before testing.

--- tools/smokeNsend.pl
+++ tools/smokeNsend.pl
@@ -1,6 +1,8 @@
 #!perl
 #
 die "Usage: username password [git-branch] [extra options for smolder_smoke_signal]. \n Found @ARGV args" if $#ARGV < 1;
+system( 'perl',  'Build.PL');
+system( 'perl', 'Build');
 system split ' ', "git pull origin $ARGV[2]" if $ARGV[2];
 system split ' ', 'prove -l -b --merge --archive sdl.tar.gz';
 system split ' ', "tools/smolder_smoke_signal smolder_smoke_signal --server sdlperl.ath.cx:8080  --username $ARGV[0] --password $ARGV[1] --file sdl.tar.gz --project SDL --architecture $] --platform $^O $ARGV[3]";
