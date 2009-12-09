888924fa28d5d63d5663ee5e97e2c30df044c6a7
Kartik Thakore <thakore.kartik@gmail.com>
2009-12-09 16:27:23 -0500

    Added net constants

--- lib/SDL/Net.pm
+++ lib/SDL/Net.pm
@@ -3,6 +3,24 @@ use strict;
 use warnings;
 require Exporter;
 require DynaLoader;
-our @ISA = qw(Exporter DynaLoader);
+use vars qw(@ISA @EXPORT);
+
+BEGIN {
+	@ISA = qw(Exporter DynaLoader);
+	@EXPORT = qw( INADDR_ANY INADDR_NONE	INADDR_BROADCAS	SDLNET_MAX_UDPCHANNELS SDLNET_MAX_UDPADDRESSES 	);      
+}
+
+
 bootstrap SDL::Net;
+
+use constant
+{
+	INADDR_ANY =>              0x00000000,
+	INADDR_NONE =>             0xFFFFFFFF,
+	INADDR_BROADCAST =>        0xFFFFFFFF,
+	SDLNET_MAX_UDPCHANNELS =>  32,
+	SDLNET_MAX_UDPADDRESSES => 4
+};
+
+
 1;
