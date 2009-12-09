655489b2d5be8780b01f549dc852417796ce90e8
Kartik Thakore <thakore.kartik@gmail.com>
2009-12-09 16:39:57 -0500

    Removed duplicate constants

--- lib/SDL/Net.pm
+++ lib/SDL/Net.pm
@@ -7,16 +7,22 @@ use vars qw(@ISA @EXPORT);
 
 BEGIN {
 	@ISA = qw(Exporter DynaLoader);
-	@EXPORT = qw( INADDR_ANY INADDR_NONE	INADDR_BROADCAS	SDLNET_MAX_UDPCHANNELS SDLNET_MAX_UDPADDRESSES 	);      
+	@EXPORT = qw(
+       		INADDR_BROADCAST
+        	SDLNET_MAX_UDPCHANNELS
+        	SDLNET_MAX_UDPADDRESSES );      
 }
 
 
 bootstrap SDL::Net;
 
+#use constant
+#{
+#	INADDR_ANY =>              0x00000000,
+#	INADDR_NONE =>             0xFFFFFFFF,
+#};
 use constant
 {
-	INADDR_ANY =>              0x00000000,
-	INADDR_NONE =>             0xFFFFFFFF,
 	INADDR_BROADCAST =>        0xFFFFFFFF,
 	SDLNET_MAX_UDPCHANNELS =>  32,
 	SDLNET_MAX_UDPADDRESSES => 4
