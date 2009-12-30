5afcdd84a0e5bd2b9d8b93ca4d9049ce699f7a3f
Kartik Thakore <thakore.kartik@gmail.com>
2009-12-30 07:43:17 -0500

    Remove other stuff from script to test only sound. Credit to ruosco

--- test/script.pl
+++ test/script.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+#CREDIT TO ruosco
+
 use strict;
 use warnings;
 use utf8;
@@ -81,125 +83,5 @@ sub setup_audio
 }
 setup_audio();
 
-our $time = $app->ticks;
-our $sevent = SDL::Event->new();
-our $timer = Event->timer
-  ( interval => (1/$FPS),
-    cb => sub {
-      my $oldtime = $time;
-      my $now = $app->ticks;
-      SDL::Events::pump_events();
-      while (SDL::Events::poll_event($sevent)) {
-        my $type = $sevent->type;
-        if ($type == SDL_QUIT()) {
-          unloop;
-        } elsif ($type == SDL_KEYDOWN() &&
-                 $sevent->key_sym() == SDLK_ESCAPE) {
-          unloop;
-        } elsif ($type == SDL_KEYDOWN() &&
-                 $sevent->key_sym() == SDLK_F11) {
-          $app->fullscreen;
-        } elsif ($type == SDL_VIDEORESIZE()) {
-          $app->resize($sevent->resize_w, $sevent->resize_h);
-          $height = $app->height;
-          $width = $app->width;
-        } else {
-          my $x = int(rand($width));
-          my $y = int(rand($height));
-          my $r = int(rand(255));
-          my $g = int(rand(255));
-          my $b = int(rand(255));
-          my $s = int(rand(20))+20;
-          my $t = int(rand(1700))+300;
-
-          my $tone = int(rand(24))-12; # produce one octave higher and
-                                       # one lower than the base
-          my $freq = 440 *             # start on A4
-            ((2 ** (1/12)) ** $tone);  # formula took from:
-          # http://www.phy.mtu.edu/~suits/NoteFreqCalcs.html
-
-          push @objects, Tecla::Object->new
-            ({ rect => SDL::Rect->new( $x,  $y,  $s, $s),
-               start_color => SDL::Color->new( $r,  $g, $b),
-               started => $time,
-               duration => $t,
-               freq => $freq });
-        }
-      }
-
-
-      @objects = grep { $_->time_lapse($oldtime,$now) } @objects;
-
-      my $rect = SDL::Rect->new(0, 0, $width, $height);
-      ;
-      SDL::Video::fill_rect($app, $rect, SDL::Video::map_RGB($app->format, 0,0,0));
-
-      $_->draw() for @objects;
-
-      SDL::Video::flip($app);
-      $time = $app->ticks;
-    });
-
-loop;
-
-exit;
-
-package Tecla::Object;
-use strict;
-use warnings;
-use base 'Class::Accessor';
-
-BEGIN {
-  __PACKAGE__->mk_accessors(qw(rect started duration time_to_live
-                               start_color color freq audio));
-}
-
-sub new {
-  my $self = shift;
-  $self = $self->SUPER::new(@_);
-  return $self;
-}
-
-sub time_lapse {
-  my ($self, $oldtime, $now) = @_;
-  $self->time_to_live(($self->started + $self->duration) - $now);
-  if ($self->time_to_live > 0) {
-    my $percent = $self->time_to_live / $self->duration;
-    $self->color
-      ( SDL::Color->new
-        (  $self->start_color->r * $percent,
-          $self->start_color->g * $percent,
-           $self->start_color->b * $percent ));
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-sub draw {
-  my $self = shift;
-  SDL::Video::fill_rect($app, $self->rect, SDL::Video::map_RGB($app->format, $self->color->r , $self->color->b, $self->color->g) );
-}
-
-
-
-1;
-
-__END__
-__C__
-
-#include <math.h>
-#include <SDL/SDL.h>
-#include <SDL/SDL_main.h>
-
-SDL_AudioSpec *desired, *obtained;
-
-void produce_audio_output(void *unused, Uint8 *stream, int len) {
-  // random noise to test the system...
-  int i;
-  for (i = 0; i < len; i++) {
-    stream[i] = rand()*120;
-  }
-}
-
-
+sleep(4);
+SDL::quit();
