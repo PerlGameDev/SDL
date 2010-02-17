847185e32c7d8524d0c54fba414bef2a19636729
unknown <miko@.dcit.cz>
2010-02-17 09:19:28 +0100

    minor loader update

--- lib/SDL/Internal/Loader.pm
+++ lib/SDL/Internal/Loader.pm
@@ -11,21 +11,28 @@ use SDL::ConfigData;
 
 sub internal_load_dlls($) {
   my $package = shift;
+
+  ### check if some ld_shlib_map is defined
+  my $shlib_map = SDL::ConfigData->config('sdl_ld_shlib_map');  
+  return unless $shlib_map; # empty shlib_map, nothing to do
+
+  ### get list of lib nicknames based on packagename
   my $lib_nick;
-  require DynaLoader;  
   if ($package =~ /^SDL(::.*)?::(.*)?$/) {
     $lib_nick = SDL::ConfigData->config('subsystems')->{$2}->{libraries};
   }
   if ($package =~ /^(SDL|SDL_perl)$/) {
     $lib_nick = [qw(SDL SDL_ttf smpeg)]; # xxx TODO xxx is the list complete?
   }
-  use Data::Dumper;
-  return unless $lib_nick;
-  my $shlib_map = SDL::ConfigData->config('sdl_ld_shlib_map');  
+  return unless $lib_nick; # no need to load anything
+
+  ### let us load the corresponding shlibs (*.dll|*.so)
+  require DynaLoader;  
   foreach my $n (@$lib_nick) {
-    $n = SDL::ConfigData->config('libraries')->{$n}->{lib};
-    my $file = $shlib_map->{$n};
-    next unless $file;
+    my $l = SDL::ConfigData->config('libraries')->{$n};
+    next unless $l; # maybe some visible warning as something went wrong
+    my $file = $shlib_map->{$l->{lib}};
+    next unless $file; # maybe some visible warning as something went wrong
     my $libref = DynaLoader::dl_load_file($file, 0);
     push(@DynaLoader::dl_librefs, $libref) if $libref;
   }
