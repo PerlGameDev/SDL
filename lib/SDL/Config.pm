97dc43e70639e2079399218d3d9c5e2172ae399d
Tobias Leich <tobias.leich@adckrone.com>
2010-03-03 14:34:16 +0100

    fixed checking for available libs

--- lib/SDL/Config.pm
+++ lib/SDL/Config.pm
@@ -8,7 +8,7 @@ sub has
 {
 	my ($class, $define) = @_;
 	my $sdl_config = SDL::ConfigData->config('SDL_cfg'); 
-	scalar grep { $$sdl_config{$_}{$define} } keys %$sdl_config;
+	scalar grep { $$sdl_config{$_}{'libs'}{$define} } keys %$sdl_config;
 }
 
 1;
