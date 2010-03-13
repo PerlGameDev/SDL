608131d13e04d778c4f248914014fe7c029ef5c8
Tobias Leich <tobias.leich@adckrone.com>
2010-03-13 15:13:47 +0100

    added SDL::GFX.pm

---
+++ lib/SDL/GFX.pm
@@ -0,0 +1,13 @@
+package SDL::GFX;
+use strict;
+use warnings;
+require Exporter;
+require DynaLoader;
+our @ISA = qw(Exporter DynaLoader);
+
+use SDL::Internal::Loader;
+internal_load_dlls(__PACKAGE__);
+
+bootstrap SDL::GFX;
+
+1;
