22835c426229d9988e328b03209d6dbd2cd3ba9d
Daniel de Oliveira Mantovani <daniel.oliveira.mantovani@gmail.com>
2010-03-22 15:58:07 -0300

    all right

--- test/testmenu.pl
+++ test/testmenu.pl
@@ -1,115 +1,78 @@
 #!/usr/bin/env perl
 
+use strict;
+use warnings;
+
 use SDL;
-use SDL::App;
-use SDL::Surface;
+use SDL::Video;
 use SDL::Rect;
+use SDL::Surface;
+use SDL::Image;
 use SDL::Event;
+use SDL::Events;
 
-my $menu = new SDL::Surface -name => 'data/menu.png';
+SDL::init(SDL_INIT_VIDEO);
 
-my $app = new SDL::App -w => $menu->width(), -h => $menu->height(), -resizeable => 1;
+my $menu = SDL::Image::load('data/menu.png');
+my $screen
+    = SDL::Video::set_video_mode( $menu->w, $menu->h, 32, SDL_SWSURFACE );
 
-my $hilight = new SDL::Surface -name => 'data/highlight.png';
+my $hilight = SDL::Image::load('data/highlight.png');
 
 my %menu = (
-	'start' => [ 115, 30, 160, 40 ],
-	'help' => [ 120, 100, 120, 40 ],
-	'giveup' => [ 120, 230, 120, 40 ],
-	'spawnserver' => [ 115, 170, 165, 40 ], 
-	'credits' => [ 115, 285, 160, 40 ],
+    'start'       => [ 115, 30,  160, 40 ],
+    'help'        => [ 120, 100, 120, 40 ],
+    'giveup'      => [ 120, 230, 120, 40 ],
+    'spawnserver' => [ 115, 170, 165, 40 ],
+    'credits'     => [ 115, 285, 160, 40 ],
 );
 
-my $needblit;
-sub drawMenu {
-	my ($a,$dx,$dy,$no,$hi,%m) = @_;
-	for (keys %m) {
-		my ($x,$y,$w,$h) = @{$m{$_}};
-		next unless $dx >= $x && $dx <= $x+$w
-			 && $dy >= $y && $dy <= $y+$h;
-		unless ($needblit) {
-			my $rect = new SDL::Rect -w => $w, -h => $h, 
-					 -x => $x, -y => $y;
-			$hi->blit($rect,$a,$rect);
-			$needblit = 1;
-		}
-		return $_;
-	}
-	$no->blit(NULL,$a,NULL) if $needblit;
-	$needblit = 0;
-	return 0;
-}
-
-sub help {
-	print STDERR <<USAGE;
-This should print a help message
-
-USAGE
-
-}
-
-sub credits {
-	print STDERR <<CREDITS;
-David J. Goehrig
-
-CREDITS
-
-}
-
-sub spawnserver {
-	print STDERR <<SPAWN;
-Spawinging new server...
-
-SPAWN
-
-}
-
-sub start  {
-	print STDERR <<START;
-This should start the game
-
-START
-
-}
-
-sub giveup {
-	print STDERR <<GIVEUP;
-Giving up
-
-GIVEUP
-
-	exit(0);
-}
-
-my %events = (
-	SDL_MOUSEMOTION() =>  sub {
-		my ($e) = @_;
-		drawMenu($app,
-			 $e->motion_x(),
-			 $e->motion_y(),
-			 $menu,
-			 $hilight,
-			 %menu);
-	},
-	SDL_MOUSEBUTTONUP() => sub {
-		my ($e) = @_;
-		my $routine = drawMenu($app,
-			 $e->motion_x(),
-			 $e->motion_y(),
-			 $menu,
-			 $hilight,
-			 %menu);
-		&{$routine} if ($routine);
-	},
-	SDL_QUIT() => sub { exit(0); },
-	SDL_KEYDOWN() => sub { 
-		my ($e) = @_;
-		exit(0) if ($e->key_sym() == SDLK_ESCAPE);
-	},
+my %item = (
+    help        => 'This should print a help message',
+    credits     => 'mantovani and kthakore',
+    spawnserver => 'Spawinging new server...',
+    start       => 'This should start the game',
+    giveup      => 'Giving up',
 );
 
-$menu->blit(NULL,$app,NULL);
-$app->sync();
-$app->loop(\%events);
+die(SDL::get_error) unless $menu;
 
+my $quit  = 0;
+my $event = SDL::Event->new();
+$event->type(SDL_ACTIVEEVENT);
+$event->active_gain(1);
+
+my $sel = 0;
+my @select = ( 'start', 'help', 'spawnserver', 'giveup', 'credits' );
+
+while ( !$quit ) {
+    while ( SDL::Events::poll_event($event) ) {
+
+        $quit = 1 if $event->type == SDL_QUIT;
+        if ( $event->type == SDL_KEYDOWN ) {
+            ### PROCESS EVENT HERE
+            if ( $event->key_sym == SDLK_DOWN ) {
+                $sel++ if $sel < $#select;
+            } elsif ( $event->key_sym == SDLK_UP ) {
+                $sel-- if $sel > 0;
+            } elsif ( $event->key_sym == 13 ) {
+                print $item{ $select[$sel] }, "\n";
+            }
+        }
+
+    }
+
+    SDL::Video::blit_surface(
+        $menu,   SDL::Rect->new( 0, 0, $menu->w,   $menu->h ),
+        $screen, SDL::Rect->new( 0, 0, $screen->w, $screen->h )
+    );
+
+    SDL::Video::blit_surface(
+        $hilight, SDL::Rect->new( @{ $menu{ $select[$sel] } } ),
+        $screen,  SDL::Rect->new( @{ $menu{ $select[$sel] } } )
+    );
+
+    SDL::Video::update_rect( $screen, 0, 0, $menu->w, $menu->h );
+
+}
 
