cb58c5979449683101bcfe73fd74925eeb042dce
Tobias Leich <tobias.leich@adckrone.com>
2010-03-26 22:47:30 +0100

    moved pango constants

--- lib/SDL/Pango.pm
+++ lib/SDL/Pango.pm
@@ -1,8 +1,10 @@
 package SDL::Pango;
 use strict;
 use warnings;
+use vars qw(@ISA @EXPORT @EXPORT_OK);
 require Exporter;
 require DynaLoader;
+use SDL::Constants ':SDL::Pango';
 our @ISA = qw(Exporter DynaLoader);
 
 use SDL::Internal::Loader;
@@ -11,38 +13,10 @@ internal_load_dlls(__PACKAGE__);
 bootstrap SDL::Pango;
 
 use base 'Exporter';
-
-our @EXPORT = qw(
-	SDLPANGO_DIRECTION_LTR
-	SDLPANGO_DIRECTION_RTL
-	SDLPANGO_DIRECTION_WEAK_LTR
-	SDLPANGO_DIRECTION_WEAK_RTL
-	SDLPANGO_DIRECTION_NEUTRAL
+our @EXPORT      = @{ $SDL::Constants::EXPORT_TAGS{'SDL::Pango'} };
+our %EXPORT_TAGS = (
+	all       => \@EXPORT,
+	direction => $SDL::Constants::EXPORT_TAGS{'SDL::Pango/direction'}
 );
-
-our %EXPORT_TAGS = 
-(
-	direction => [qw(
-		SDLPANGO_DIRECTION_LTR
-		SDLPANGO_DIRECTION_RTL
-		SDLPANGO_DIRECTION_WEAK_LTR
-		SDLPANGO_DIRECTION_WEAK_RTL
-		SDLPANGO_DIRECTION_NEUTRAL
-	)]
-);
-
-use constant{
-	SDLPANGO_DIRECTION_LTR      => 0,
-	SDLPANGO_DIRECTION_RTL      => 1,
-	SDLPANGO_DIRECTION_WEAK_LTR => 2,
-	SDLPANGO_DIRECTION_WEAK_RTL => 3,
-	SDLPANGO_DIRECTION_NEUTRAL  => 4
-}; # SDLPango_Direction
-
-# add all the other ":class" tags to the ":all" class,
-# deleting duplicates
-my %seen;
-push @{$EXPORT_TAGS{all}},
-grep {!$seen{$_}++} @{$EXPORT_TAGS{$_}} foreach keys %EXPORT_TAGS;
 
 1;
--- lib/SDL/Pango/Context.pm
+++ lib/SDL/Pango/Context.pm
@@ -1,13 +1,22 @@
 package SDL::Pango::Context;
 use strict;
 use warnings;
+use vars qw(@ISA @EXPORT @EXPORT_OK);
 require Exporter;
 require DynaLoader;
+use SDL::Constants ':SDL::Pango';
 our @ISA = qw(Exporter DynaLoader);
 
 use SDL::Internal::Loader;
 internal_load_dlls(__PACKAGE__);
 
 bootstrap SDL::Pango::Context;
+
+use base 'Exporter';
+our @EXPORT      = @{ $SDL::Constants::EXPORT_TAGS{'SDL::Pango'} };
+our %EXPORT_TAGS = (
+	all       => \@EXPORT,
+	direction => $SDL::Constants::EXPORT_TAGS{'SDL::Pango/direction'}
+);
 
 1;
