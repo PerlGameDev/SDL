53b0532e294a6b6642f15ad9c507d5ebc1d76762
Tobias Leich <tobias.leich@adckrone.com>
2010-03-26 22:12:07 +0100

    added exporting video constants for the video family

--- lib/SDL/VideoInfo.pm
+++ lib/SDL/VideoInfo.pm
@@ -1,13 +1,28 @@
 package SDL::VideoInfo;
 use strict;
 use warnings;
+use vars qw(@ISA @EXPORT @EXPORT_OK);
 require Exporter;
 require DynaLoader;
+use SDL::Constants ':SDL::Video';
 our @ISA = qw(Exporter DynaLoader);
 
 use SDL::Internal::Loader;
 internal_load_dlls(__PACKAGE__);
 
 bootstrap SDL::VideoInfo;
+
+use base 'Exporter';
+our @EXPORT      = @{ $SDL::Constants::EXPORT_TAGS{'SDL::Video'} };
+our %EXPORT_TAGS = (
+	all     => \@EXPORT,
+	color   => $SDL::Constants::EXPORT_TAGS{'SDL::Video/color'},
+	surface => $SDL::Constants::EXPORT_TAGS{'SDL::Video/surface'},
+	video   => $SDL::Constants::EXPORT_TAGS{'SDL::Video/video'},
+	overlay => $SDL::Constants::EXPORT_TAGS{'SDL::Video/overlay'},
+	grab    => $SDL::Constants::EXPORT_TAGS{'SDL::Video/grab'},
+	palette => $SDL::Constants::EXPORT_TAGS{'SDL::Video/palette'},
+	gl      => $SDL::Constants::EXPORT_TAGS{'SDL::Video/gl'}
+);
 
 1;
