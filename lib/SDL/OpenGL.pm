6165c387f5cc46ec960040962962f9c7dbb1689e
Tobias Leich <tobias.leich@adckrone.com>
2010-03-27 00:55:18 +0100

    moved opengl constants

--- lib/SDL/OpenGL.pm
+++ lib/SDL/OpenGL.pm
@@ -1,66 +1,36 @@
-#!/usr/bin/env perl
-#
-# OpenGL.pm
-#
-# Copyright (C) 2005 David J. Goehrig <dgoehrig@cpan.org>
-#
-# ------------------------------------------------------------------------------
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-# 
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-# 
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-#
-# ------------------------------------------------------------------------------
-#
-# Please feel free to send questions, suggestions or improvements to:
-#
-#	David J. Goehrig
-#	dgoehrig@cpan.org
-#
-
 package SDL::OpenGL;
-
 use strict;
 use warnings;
-use Carp;
-
+use vars qw(@ISA @EXPORT @EXPORT_OK);
 require Exporter;
 require DynaLoader;
-use vars qw(
-	@EXPORT
-	@ISA
-);
-@ISA=qw(Exporter DynaLoader);
+use SDL::Constants ':SDL::Video';
+use SDL::OpenGL::Constants;
+our @ISA = qw(Exporter DynaLoader);
 
-use SDL;
+use SDL::Internal::Loader;
+internal_load_dlls(__PACKAGE__);
 
 bootstrap SDL::OpenGL;
+
+use base 'Exporter';
+our @EXPORT      = (@{ $SDL::Constants::EXPORT_TAGS{'SDL::Video'} }, @SDL::OpenGL::Constants::EXPORT);
 for ( keys %SDL::OpenGL:: ) {
 	if (/^gl/) {
 		push @EXPORT,$_;
 	}
 }
-
-use SDL::OpenGL::Constants;
-
-sub import {
-	  my $self = shift;
-	   
-	    $self->export_to_level(1, @_);
-      SDL::OpenGL::Constants->export_to_level(1);
-      }
-
-
+our %EXPORT_TAGS = (
+	all     => \@EXPORT,
+	color   => $SDL::Constants::EXPORT_TAGS{'SDL::Video/color'},
+	surface => $SDL::Constants::EXPORT_TAGS{'SDL::Video/surface'},
+	video   => $SDL::Constants::EXPORT_TAGS{'SDL::Video/video'},
+	overlay => $SDL::Constants::EXPORT_TAGS{'SDL::Video/overlay'},
+	grab    => $SDL::Constants::EXPORT_TAGS{'SDL::Video/grab'},
+	palette => $SDL::Constants::EXPORT_TAGS{'SDL::Video/palette'},
+	gl      => $SDL::Constants::EXPORT_TAGS{'SDL::Video/gl'},
+	opengl  => \@SDL::OpenGL::Constants::EXPORT,
+);
 
 1;
 
