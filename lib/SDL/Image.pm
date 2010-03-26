2bb8295734a1fcea5e373fff95acbe8aed6f36e6
Tobias Leich <tobias.leich@adckrone.com>
2010-03-27 00:54:36 +0100

    moved image constants

--- lib/SDL/Image.pm
+++ lib/SDL/Image.pm
@@ -1,26 +1,22 @@
 package SDL::Image;
 use strict;
 use warnings;
+use vars qw(@ISA @EXPORT @EXPORT_OK);
 require Exporter;
 require DynaLoader;
-use vars qw(@ISA @EXPORT);
-
-BEGIN {
-  @ISA = qw(Exporter DynaLoader);
-  @EXPORT = qw( IMG_INIT_JPG IMG_INIT_PNG IMG_INIT_TIF);      
-}
+use SDL::Constants ':SDL::Image';
+our @ISA = qw(Exporter DynaLoader);
 
 use SDL::Internal::Loader;
 internal_load_dlls(__PACKAGE__);
 
 bootstrap SDL::Image;
 
-use constant
-{
-   IMG_INIT_JPG => 0x00000001,
-   IMG_INIT_PNG => 0x00000002,
-   IMG_INIT_TIF => 0x00000004,
-};
-
+use base 'Exporter';
+our @EXPORT      = @{ $SDL::Constants::EXPORT_TAGS{'SDL::Image'} };
+our %EXPORT_TAGS = (
+	all  => \@EXPORT,
+	init => $SDL::Constants::EXPORT_TAGS{'SDL::Video/init'}
+);
 
 1;
