28b7a2d33bc9735b6dd6b91bd3ec77f96109c2da
Kartik Thakore <thakore.kartik@gmail.com>
2010-04-09 14:34:19 +0000

    Updated to 2.42

--- lib/SDL.pm
+++ lib/SDL.pm
@@ -52,7 +52,7 @@ our %EXPORT_TAGS = (
 	defaults => $SDL::Constants::EXPORT_TAGS{'SDL/defaults'}
 );
 
-our $VERSION = '2.4';
+our $VERSION = '2.42';
 $VERSION = eval $VERSION;
 
 print "$VERSION" if (defined($ARGV[0]) && ($ARGV[0] eq '--SDLperl'));
