21d10924242cbb4ba58b121ce4918ae43b2face6
Kartik Thakore <thakore.kartik@gmail.com>
2010-06-23 18:29:22 +0000

    Update to fix references but still not working. from http://perlmonks.com/?node_id=846128

--- examples/SDLx_exp.pl
+++ examples/SDLx_exp.pl
@@ -4,6 +4,7 @@ use SDL::Video;
 use SDL::Surface;
 use SDLx::Surface;
 use SDL::Rect;
+#use Tie::Array;
 
 use Devel::Peek;
 use Data::Dumper;
@@ -12,63 +13,55 @@ use Data::Dumper;
 my $screen_width  = 800;
 my $screen_height = 600;
 
-SDL::init(SDL_INIT_VIDEO);
+#SDL::init(SDL_INIT_VIDEO);
 
 # setting video mode
 my $screen_surface =
-  SDL::Video::set_video_mode( $screen_width, $screen_height, 32,
-    SDL_ANYFORMAT );
+  SDL::Surface->new( SDL_ANYFORMAT, 30, 30, 32, 0, 0, 0, 0);
+     
 
 #$s = SDL::Image::load('hero.png');
     my $mapped_color =
-      SDL::Video::map_RGB( $screen_surface->format(), 0, 0, 255 );    # blue
+      SDL::Video::map_RGB( $screen_surface->format(), 0, 255, 0 );    # blue
 
-       SDL::Video::fill_rect( $screen_surface,
+        SDL::Video::fill_rect( $screen_surface,
         SDL::Rect->new( 0, 0, $screen_width, $screen_height ),
         $mapped_color );
 
 
-
-clear();
-
-
-
 my $surf32_matrix = SDLx::Surface::pixel_array($screen_surface);
 
-print Dump $surf32_matrix->[1][1]; 
-print "\n";
-print unpack 'L', $surf32_matrix->[1][1]; 
+my $d = sprintf( "#### %p %p", $surf32_matrix->[1]->[1], $surf32_matrix->[1][1]);
+warn $d;
+warn  "BEFORE \n";
+foo();
 
-
-
-foreach(0..100) {
+warn "\nAFTER \n";
 if ( SDL::Video::MUSTLOCK($screen_surface) ) {
     return if ( SDL::Video::lock_surface($screen_surface) < 0 );
 }
-	  
-	  ${$surf32_matrix->[$_][$_]} = pack 'L', 0x000000FF;
+# 
+ my $green = pack 'l*', '11111111000000000000000000000000';
+my $ref =$surf32_matrix->[0][0];
+vec($$ref, 0, 32) = 0x34343434; 
 
-	  print unpack 'L', $surf32_matrix->[$_][$_];
+ foo("vec direct");
 
-	SDL::Video::unlock_surface($screen_surface) if ( SDL::Video::MUSTLOCK($screen_surface) );
-
-	clear();
-	SDL::delay(10);
- }
+substr($$ref, 0, 4) = pack('N', 0x33333333);  # OK
+# 
+ foo("substr pack");
 
 
+SDL::Video::unlock_surface($screen_surface)
+  if ( SDL::Video::MUSTLOCK($screen_surface) );
 
-sub clear {
 
-    # drawing something somewhere
-#    SDL::Video::blit_surface( $s, SDL::Rect->new( 0, 0, $s->w, $s->h), $screen_surface, SDL::Rect->new( 0, 0, $screen_width, $screen_height ));
-    # update an area on the screen so its visible
-    SDL::Video::update_rect( $screen_surface, 0, 0, $screen_width, $screen_height );
 
+
+sub foo
+{
+  warn "AT (0,0) using $_[0] \n";
+  print Dump $surf32_matrix->[0][0]; 
+  warn unpack 'l*', $surf32_matrix->[0][0];
+  
 }
-
-
-
-clear();
-
-
--- src/SDLx/Surface.xs
+++ src/SDLx/Surface.xs
@@ -11,24 +11,30 @@
 
 SV * get_pixel32 (SDL_Surface *surface, int x, int y)
 {
+	
+	if( x==0 && y ==0)
+	{
+		
+	}
 	//Convert the pixels to 32 bit 
 	Uint32 *pixels = (Uint32 *)surface->pixels; 
 	//Get the requested pixel 
 	
-	Uint32* u_ptr =  pixels + ( y * surface->w ) + x ; 
+	Uint32* u_ptr =  &(pixels[( y * surface->w ) + x ]); 
 
 
-	SV* sv = newSVpv("a",1);
+	SV* sv = newSV_type(SVt_PV);
+        SvPV_set(sv, (char*)u_ptr);
+        SvLEN_set(sv, 0);              /* So Perl won't free it. */
         SvCUR_set(sv, sizeof(Uint32));
-	SvLEN_set(sv, sizeof(Uint32));
-        SvPV_set(sv,(char*)u_ptr);
+        SvPOK_on(sv);
 
 	return sv; //make a modifiable reference using u_ptr's place as the memory :)
 
 }
 
 
-AV * construct_p_matrix ( SDL_Surface *surface )
+SV * construct_p_matrix ( SDL_Surface *surface )
 {
     AV * matrix = newAV();
      int i, j;
@@ -42,7 +48,7 @@ AV * construct_p_matrix ( SDL_Surface *surface )
 	  av_push(matrix, newRV_noinc((SV*) matrix_row) );
 	}
 
-	return matrix;
+	return newRV_noinc(matrix);
 }
 
 
@@ -51,7 +57,7 @@ AV * construct_p_matrix ( SDL_Surface *surface )
 MODULE = SDLx::Surface 	PACKAGE = SDLx::Surface    PREFIX = surfacex_
 
 
-AV *
+SV *
 surfacex_pixel_array ( surface )
 	SDL_Surface *surface
 	CODE:
@@ -60,4 +66,3 @@ surfacex_pixel_array ( surface )
 		
 	OUTPUT:
 		RETVAL
-
