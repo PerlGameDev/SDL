4a88eabfd919203628624ec6b03cc9d7e0859eb4
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2010-07-19 19:54:49 -0400

    Tidy'd.

--- examples/SDLx/SDLx_sprite_animated.pl
+++ examples/SDLx/SDLx_sprite_animated.pl
@@ -8,37 +8,36 @@ use SDL::GFX::Rotozoom;
 
 use SDLx::Sprite::Animated;
 
-
-
 SDL::init(SDL_INIT_VIDEO);
 
-my $disp = SDL::Video::set_video_mode( 300, 300, 32, SDL_ANYFORMAT);
+my $disp = SDL::Video::set_video_mode( 300, 300, 32, SDL_ANYFORMAT );
 
-my $pixel   = SDL::Video::map_RGB( $disp->format, 0, 0, 0 );
-SDL::Video::fill_rect( $disp, SDL::Rect->new( 0, 0, $disp->w, $disp->h ), $pixel );
-
+my $pixel = SDL::Video::map_RGB( $disp->format, 0, 0, 0 );
+SDL::Video::fill_rect( $disp, SDL::Rect->new( 0, 0, $disp->w, $disp->h ),
+    $pixel );
 
 my $sprite = SDLx::Sprite::Animated->new(
-       	image => 'test/data/hero.png',
-	rect  => SDL::Rect->new(48,0,48,48),
-	ticks_per_frame => 6, 
-					);
- $sprite->set_sequences(
-	 left => [ [ 1, 0 ], [1,1], [1,2] ],
- );
- $sprite->alpha_key(SDL::Color->new(0xfc, 0x00, 0xff));
- $sprite->current_sequence('left'); 
- $sprite->start();
- my $x = 0; my $ticks = 0;
- while ($x++ < 30) {
-   SDL::Video::fill_rect( $disp, SDL::Rect->new( 0, 0, $disp->w, $disp->h ), $pixel );
+    image           => 'test/data/hero.png',
+    rect            => SDL::Rect->new( 48, 0, 48, 48 ),
+    ticks_per_frame => 6,
+);
+$sprite->set_sequences( left => [ [ 1, 0 ], [ 1, 1 ], [ 1, 2 ] ], );
+$sprite->alpha_key( SDL::Color->new( 0xfc, 0x00, 0xff ) );
+$sprite->current_sequence('left');
+$sprite->start();
+my $x     = 0;
+my $ticks = 0;
 
-     $sprite->x($x * 10);
-     $sprite->next();
-     $sprite->draw($disp);
+while ( $x++ < 30 ) {
+    SDL::Video::fill_rect( $disp, SDL::Rect->new( 0, 0, $disp->w, $disp->h ),
+        $pixel );
 
-     SDL::Video::update_rect($disp, 0, 0, 0, 0);
+    $sprite->x( $x * 10 );
+    $sprite->next();
+    $sprite->draw($disp);
 
-     SDL::delay( 100 );
- }
+    SDL::Video::update_rect( $disp, 0, 0, 0, 0 );
+
+    SDL::delay(100);
+}
 
