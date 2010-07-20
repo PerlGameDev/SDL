8acd38e059f5cfe2a0426eb2cebc7af99000440e
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2010-07-19 22:21:39 -0400

    Better clip updating.  Using load after new works.

--- lib/SDLx/Sprite/Animated.pm
+++ lib/SDLx/Sprite/Animated.pm
@@ -15,10 +15,15 @@ use base 'SDLx::Sprite';
 sub new {
     my ( $class, %options ) = @_;
 
-    my ( $w, $h ) = ( $options{rect}->w, $options{rect}->h )
-        if exists $options{rect};
+    if ( exists $options{rect} ) {
+        $options{width}  = $options{rect}->w;
+        $options{height} = $options{rect}->h;
+    }
+    my ( $w, $h ) = ( $options{width}, $options{height} );
 
     my $self = $class->SUPER::new(%options);
+
+    $self->_store_geometry( $w, $h );
 
     $self->step_y( exists $options{step_y} ? $options{step_y} : 0 );
     $self->step_x( exists $options{step_x} ? $options{step_x} : 0 );
@@ -27,20 +32,35 @@ sub new {
     $self->ticks_per_frame(
         exists $options{ticks_per_frame} ? $options{ticks_per_frame} : 0 );
 
-    if ( exists $options{rect} ) {
-        $self->rect->w($w);
-        $self->rect->h($h);
-        $self->clip->w($w);
-        $self->clip->h($h);
-    }
-
     $self->{timer} = SDLx::Controller::Timer->new();
+
     return $self;
 }
 
 sub load {
     my $self = shift;
-    return $self->SUPER::load(@_);
+
+    $self->SUPER::load(@_);
+
+    $self->_restore_geometry;
+
+    return $self;
+}
+
+sub _store_geometry {
+    my ( $self, $w, $h ) = @_;
+
+    $self->{width}  = $w;
+    $self->{height} = $h;
+}
+
+sub _restore_geometry {
+    my $self = shift;
+
+    $self->clip->w( $self->{width} );
+    $self->clip->h( $self->{height} );
+    $self->rect->w( $self->{width} );
+    $self->rect->h( $self->{height} );
 }
 
 sub step_y {
@@ -141,12 +161,14 @@ sub next {
     # TODO direction, type, max_loops, current_loop
     $self->{current_frame}
         = ( $self->{current_frame} + 1 ) % @{ $self->_sequence };
+    $self->_update_clip;
 }
 
 sub previous {
     my $self = shift;
 
     # TODO
+    $self->_update_clip;
     return $self;
 }
 
@@ -170,18 +192,15 @@ sub stop {
     return $self;
 }
 
-sub _source_frame {
+sub _update_clip {
     my $self = shift;
 
-    # TODO step_x, step_y
     my $clip  = $self->clip;
     my $frame = $self->_frame;
 
-    return SDL::Rect->new(
-        $frame->[0] * $clip->w,
-        $frame->[1] * $clip->h,
-        $clip->w, $clip->h,
-    );
+    # TODO step_x, step_y
+    $clip->x( $frame->[0] * $clip->w );
+    $clip->y( $frame->[1] * $clip->h );
 }
 
 sub draw {
@@ -201,7 +220,7 @@ sub draw {
     Carp::croak 'destination must be a SDL::Surface'
         unless ref $surface and $surface->isa('SDL::Surface');
 
-    SDL::Video::blit_surface( $self->surface, $self->_source_frame, $surface,
+    SDL::Video::blit_surface( $self->surface, $self->clip, $surface,
         $self->rect );
     return $self;
 }
