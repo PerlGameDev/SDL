2845fee89589394ea906721981bc9a243298adc9
Kartik Thakore <thakore.kartik@gmail.com>
2010-07-27 19:09:18 -0400

    Fixed the example for app.pl

--- examples/SDLx/app.pl
+++ examples/SDLx/app.pl
@@ -2,19 +2,19 @@ use SDL::Event;
 use SDLx::App;
 
 my $app = SDLx::App->new(
-		title  => "Test",
+		title  => "Lines",
 		width  => 640,
 		height => 480,
 		);
 
 
 
-sub draw_line { $app->draw_line([0,1], [10,10], 0xFFFFFFFF); $app->update(); }
+sub draw_line { $app->draw_line([0,0], [rand($app->w), rand($app->h)], 0xFFFFFFFF); $app->update(); }
 
 sub event_handle { my $e = shift; return  if($e->type == SDL_QUIT); return 1}
 
-$app->add_event_handler( &event_handle );
-$app->add_show_handler( &draw_line );
+$app->add_event_handler( \&event_handle );
+$app->add_show_handler( \&draw_line );
 
 $app->run();
 
