c6ab45d19940a12662dc37db75ca2c3997e77eee
Kartik Thakore <thakore.kartik@gmail.com>
2010-07-27 21:06:21 -0400

    The sub name was horrible. Still is bad but getting better

--- examples/SDLx/app.pl
+++ examples/SDLx/app.pl
@@ -9,12 +9,12 @@ my $app = SDLx::App->new(
 
 
 
-sub draw_line { $app->draw_line([0,0], [rand($app->w), rand($app->h)], 0xFFFFFFFF); $app->update(); }
+sub draw_lines { $app->draw_line([0,0], [rand($app->w), rand($app->h)], 0xFFFFFFFF); $app->update(); }
 
 sub event_handle { my $e = shift; return  if($e->type == SDL_QUIT); return 1}
 
 $app->add_event_handler( \&event_handle );
-$app->add_show_handler( \&draw_line );
+$app->add_show_handler( \&draw_lines );
 
 $app->run();
 
