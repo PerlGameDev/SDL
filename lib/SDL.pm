6baf80a7d6aad388875f72afda6c71d3a331b9ae
kthakore <thakore.kartik@gmail.com>
2010-08-06 15:30:20 +0000

    Prepared for release 2.511

--- lib/SDL.pm
+++ lib/SDL.pm
@@ -54,7 +54,7 @@ our %EXPORT_TAGS = (
 	defaults => $SDL::Constants::EXPORT_TAGS{'SDL/defaults'}
 );
 
-our $VERSION = '2.510';
+our $VERSION = '2.511';
 $VERSION = eval $VERSION;
 
 print "$VERSION" if ( defined( $ARGV[0] ) && ( $ARGV[0] eq '--SDLperl' ) );
