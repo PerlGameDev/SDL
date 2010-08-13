2bf98303db648927ce349da9a3885ca01f84c571
Tobias Leich <tobias.leich@adckrone.com>
2010-08-13 22:33:44 +0200

    fixed behind function

--- src/SDLx/LayerManager.h
+++ src/SDLx/LayerManager.h
@@ -51,29 +51,29 @@ int intersection( SDLx_Layer *layer1, SDLx_Layer *layer2 )
 {
     if(
         // upper left point inside layer
-        (      layer1->pos->x <= layer2->pos->x
-            && layer2->pos->x <= layer1->pos->x + layer1->clip->w
-            && layer1->pos->y <= layer2->pos->y
-            && layer2->pos->y <= layer1->pos->y + layer1->clip->h
+        (      layer1->pos->x < layer2->pos->x
+            && layer2->pos->x < layer1->pos->x + layer1->clip->w
+            && layer1->pos->y < layer2->pos->y
+            && layer2->pos->y < layer1->pos->y + layer1->clip->h
         )
 
         // upper right point inside layer
-        || (   layer1->pos->x <= layer2->pos->x + layer2->clip->w
-            && layer2->pos->x + layer2->clip->w <= layer1->pos->x + layer1->clip->w
-            && layer1->pos->y <= layer2->pos->y
-            && layer2->pos->y <= layer1->pos->y + layer1->clip->h )
+        || (   layer1->pos->x < layer2->pos->x + layer2->clip->w
+            && layer2->pos->x + layer2->clip->w < layer1->pos->x + layer1->clip->w
+            && layer1->pos->y < layer2->pos->y
+            && layer2->pos->y < layer1->pos->y + layer1->clip->h )
 
         // lower left point inside layer
-        || (   layer1->pos->x <= layer2->pos->x
-            && layer2->pos->x <= layer1->pos->x + layer1->clip->w
-            && layer1->pos->y <= layer2->pos->y + layer2->clip->h
-            && layer2->pos->y + layer2->clip->h <= layer1->pos->y + layer1->clip->h )
+        || (   layer1->pos->x < layer2->pos->x
+            && layer2->pos->x < layer1->pos->x + layer1->clip->w
+            && layer1->pos->y < layer2->pos->y + layer2->clip->h
+            && layer2->pos->y + layer2->clip->h < layer1->pos->y + layer1->clip->h )
 
         // lower right point inside layer
-        || (   layer1->pos->x <= layer2->pos->x + layer2->clip->w
-            && layer2->pos->x + layer2->clip->w <= layer1->pos->x + layer1->clip->w
-            && layer1->pos->y <= layer2->pos->y + layer2->clip->h
-            && layer2->pos->y + layer2->clip->h <= layer1->pos->y + layer1->clip->h )
+        || (   layer1->pos->x < layer2->pos->x + layer2->clip->w
+            && layer2->pos->x + layer2->clip->w < layer1->pos->x + layer1->clip->w
+            && layer1->pos->y < layer2->pos->y + layer2->clip->h
+            && layer2->pos->y + layer2->clip->h < layer1->pos->y + layer1->clip->h )
     ){
         return 1;
     }
@@ -91,7 +91,7 @@ AV *layers_behind( SDLx_Layer *layer )
     {
         SV *bag            = *av_fetch(layer->manager->layers, i, 0);
         SDLx_Layer *layer2 = bag_to_layer(bag);
-        if(intersection( layer, layer2 ))
+        if(intersection( layer, layer2 ) || intersection( layer2, layer ))
         {
             // TODO checking transparency
             SvREFCNT_inc(bag);
@@ -100,6 +100,19 @@ AV *layers_behind( SDLx_Layer *layer )
         }
     }
     
+    if(count)
+    {
+        AV *behind = layers_behind(bag_to_layer(*av_fetch(matches, av_len(matches), 0)));
+        if(av_len(behind) >= 0)
+        {
+            for( i = 0; i <= av_len(behind); i++ )
+            {
+                av_store( matches, count, *av_fetch(behind, i, 0));
+                count++;
+            }
+        }
+    }
+
     return matches;
 }
 
@@ -113,7 +126,7 @@ AV *layers_ahead( SDLx_Layer *layer )
     {
         SV *bag            = *av_fetch(layer->manager->layers, i, 0);
         SDLx_Layer *layer2 = bag_to_layer(bag);
-        if(intersection( layer, layer2 ))
+        if(intersection( layer, layer2 ) || intersection( layer2, layer ))
         {
             // TODO checking transparency
             SvREFCNT_inc(bag);
