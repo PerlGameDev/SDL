e7a2ce7ce2a60347cb59e56034bc90b5174115b5
kthakore <thakore.kartik@gmail.com>
2010-10-03 22:36:49 -0400

    app.pl is done

--- examples/SDLx/app.pl
+++ examples/SDLx/app.pl
@@ -11,7 +11,7 @@ my $app = SDLx::App->new(
 
 sub draw_lines { $app->draw_line( [ 0, 0 ], [ rand( $app->w ), rand( $app->h ) ], 0xFFFFFFFF ); $app->update(); }
 
-sub event_handle { my $e = shift; return if ( $e->type == SDL_QUIT ); return 1 }
+sub event_handle { my $e = shift; $_[0]->stop if ( $e->type == SDL_QUIT ); }
 
 $app->add_event_handler( \&event_handle );
 $app->add_show_handler( \&draw_lines );
