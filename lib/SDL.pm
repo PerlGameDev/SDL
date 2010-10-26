7822cb5c173870e2be3b64ea0e593a82d20abb32
kthakore <thakore.kartik@gmail.com>
2010-10-26 07:02:50 -0400

    Didn't perform manifest before releasing SDL 2.520_1. New release at 2.520_2

--- lib/SDL.pm
+++ lib/SDL.pm
@@ -54,7 +54,7 @@ our %EXPORT_TAGS = (
 	defaults => $SDL::Constants::EXPORT_TAGS{'SDL/defaults'}
 );
 
-our $VERSION = '2.520_1';
+our $VERSION = '2.520_2';
 $VERSION = eval $VERSION;
 
 print "$VERSION" if ( defined( $ARGV[0] ) && ( $ARGV[0] eq '--SDLperl' ) );
