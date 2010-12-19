60e2e24fbd3d80b08df93d7712ab605dd5233235
kmx <kmx@cpan.org>
2010-12-19 22:01:39 +0100

    Module::Build::SDL - initial documentation

--- lib/Module/Build/SDL.pm
+++ lib/Module/Build/SDL.pm
@@ -3,17 +3,8 @@ use strict;
 use warnings;
 use base 'Module::Build';
 
-
-#Makes the default directory stucture for SDL
-#
-# $path
-#    /lib
-#	 /bin
-#    /bin/sdl_app.pl
-#    /data
-# 	 Build.PL
-#
-sub generate_sdl_module{
+# TODO: later move app skeleton generation into SDL::Devel (or something like this)
+sub generate_sdl_module {
 	my ($path, $name) = @_;
 
 	#Make the path and directory stuff
@@ -46,3 +37,114 @@ sub generate_sdl_module{
 	
 }
 1;
+
+__END__
+
+=head1 NAME
+
+Module::Build::SDL - Module::Build subclass for building SDL apps/games [not stable yet]
+
+=head1 SYNOPSIS
+
+When creating a new SDL application/game you can create Build.PL like this:
+
+ use Module::Build::SDL;
+   
+ my $builder = Module::Build::SDL->new(
+     module_name   => 'Games::Demo',
+     dist_version  => '1.00',
+     dist_abstract => 'Demo game based on Module::Build::SDL',
+     dist_author   => 'coder@cpan.org',
+     license       => 'perl',
+     requires      => {
+         'SDL'     => 0,
+     },
+     #+ others Module::Build options
+ )->create_build_script();
+
+Once you have created a SDL application/game via Module::Build::SDL as described
+above you can use some extra build targets/actions:
+
+=over
+
+=item * you can create a PAR distribution like:
+
+ $ perl ./Build.PL
+ $ ./Build
+ $ ./Build par
+
+=item * to run the game from distribution directory you can use:
+
+ $ perl ./Build.PL
+ $ ./Build
+ $ ./Build run
+
+=item * TODO: maybe some additional actions: parexe, parmsi, deb, rpm
+
+=back
+
+=head1 DESCRIPTION
+
+Module::Build::SDL is a subclass of L<Module::Build|Module::Build> created
+to make easy some tasks specific to SDL applications - e.g. packaging SDL
+application/game into PAR archive.
+
+=head1 APPLICATION/GAME LAYOUT
+
+Module::Build::SDL expects the following layout in project directory:
+
+ #example: game with the main *.pl script + data files + modules (*.pm)
+ Build.PL
+ lib/
+     Games/
+           Demo.pm
+ bin/
+     game-script.pl
+ data/
+     whatever_data_files_you_need.jpg
+
+the most simple game should look like:
+
+ #example: simple one-script apllication/game
+ Build.PL
+ bin/
+    game-script.pl
+
+In short - there are 3 expected subdirectories:
+
+=over
+
+=item * B<bin> - one or more perl scripts (*.pl) to start the actual
+application/game
+
+=item * B<lib> - application/game specific modules (*.pm) organized
+in dir structure in "usual perl manners"
+
+=item * B<data> - directory for storing application data (pictures, 
+sounds etc.). This subdirectory is handled as a "ShareDir"
+(see L<File::ShareDir|File::ShareDir> for more details)
+
+=item * As the project is (or could be) composed as a standard perl
+distribution it also support standard subdirectory B<'t'> (with tests).
+
+=back
+
+=head1 RULES TO FOLLOW
+
+When creating a SDL application/game based on Module::Build::SDL it is
+recommended to follow these rules:
+
+=over
+
+=item * Use the name for your game from I<Games::*> namespace; it will make
+the later release to CPAN much easier.
+
+=item * Put all data files into B<data> subdirectory and access the B<data>
+subdir only via L<File::ShareDir|File::ShareDir> 
+(namely by calling L<distdir()|File::ShareDir/dist_dir> function)
+
+=item * TODO: maybe add more
+
+=back
+
+=cut
