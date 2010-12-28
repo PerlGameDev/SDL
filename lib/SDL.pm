5dea3cb94130e529a63a37f726949b2f1b28f5c3
kthakore <thakore.kartik@gmail.com>
2010-12-28 14:36:13 -0500

    Fixing silling regex check for windows 32

--- lib/SDL.pm
+++ lib/SDL.pm
@@ -104,7 +104,7 @@ sub Inline
     require File::Spec;
 	my $libs = Alien::SDL->config('libs');
 	#This should be added in ldd flags section but it is only doing SDL_main for some reason.
-	$libs .= ' '.File::Spec->catfile(Alien::SDL->config('prefix'),'lib','libSDL.dll.a')	if( $^O =~ 'win32' );
+	$libs .= ' '.File::Spec->catfile(Alien::SDL->config('prefix'),'lib','libSDL.dll.a')	if( $^O =~ /Win32/ig );
 	my $cflags = Alien::SDL->config('cflags');
 	my $path;
 	my $sdl_typemap = File::Spec->catfile( 'SDL', 'typemap' );
