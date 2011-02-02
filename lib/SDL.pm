954ebe2ee6969214eff51097ecdd3f81cb98f01f
Tobias Leich <tobias.leich@adckrone.com>
2011-02-02 17:56:09 +0100

    version is now 2.530, requiring Alien::SDL 1.424

--- lib/SDL.pm
+++ lib/SDL.pm
@@ -54,7 +54,7 @@ our %EXPORT_TAGS = (
 	defaults => $SDL::Constants::EXPORT_TAGS{'SDL/defaults'}
 );
 
-our $VERSION = '2.529_1';
+our $VERSION = '2.530';
 $VERSION = eval $VERSION;
 
 print "$VERSION" if ( defined( $ARGV[0] ) && ( $ARGV[0] eq '--SDLperl' ) );
