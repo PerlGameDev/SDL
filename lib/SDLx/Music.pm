f9528a8a11ce3b3909c3df58fd130ed753b606e7
kthakore <thakore.kartik@gmail.com>
2011-04-20 21:10:23 -0400

    Adding optional and default args for new

--- lib/SDLx/Music.pm
+++ lib/SDLx/Music.pm
@@ -12,45 +12,49 @@ use SDL::Mixer::Channels;
 use SDL::Mixer::Samples;
 use SDL::Mixer::MixChunk;
 
-
 sub new {
-	my $class = shift;
-	my %params = @_;
+    my $class  = shift;
+    my %params = @_;
 
-	my $self = bless { %params }, $class;
+    my $self = bless {%params}, $class;
 
-	# Initialize Audio 
+    # Initialize Audio
+    $self->{freq}      = $self->{freq} || 44100;
+    $self->{format}    = $self->{format} || SDL::Audio::AUDIO_S16SYS;
+    $self->{channels}  = $self->{channels} || 2;
+    $self->{chunksize} = $self->{chunksize} || 4096;
 
-	Carp::croak SDL::get_error() if ( SDL::Mixer::open_audio( 44100, SDL::Audio::AUDIO_S16SYS, 2, 4096) ) != 0 ;
-
+    Carp::croak SDL::get_error()
+      if ( SDL::Mixer::open_audio( $self->{freq}, $self->{format}, $self->{channels}, $self->{chunksize} ) )
+      != 0;
 
     return $self;
 }
 
 sub data {
-	my $self = shift;
-	my %data = @_;
-	# loop through keys
-    foreach( keys %data )
-	{
-		my $datum = $data{$_};
+    my $self = shift;
+    my %data = @_;
 
-		#If SCALAR is Simple
-		 if( defined $datum )
-			{
-  				 if ( ref $datum eq 'HASH' ) { 
-					$self->{data}->{$_} = $datum;
-					$self->{data}->{$_}->{_content} = SDL::Mixer::Music::load_MUS( $datum->{file} );
-					} 
-				 else{ 
-					$self->{data}->{$_}->{_content} = SDL::Mixer::Music::load_MUS( $datum );
-				 } 
-			}
-				
+    # loop through keys
+    foreach ( keys %data ) {
+        my $datum = $data{$_};
 
-	}
+        #If SCALAR is Simple
+        if ( defined $datum ) {
+            if ( ref $datum eq 'HASH' ) {
+                $self->{data}->{$_} = $datum;
+                $self->{data}->{$_}->{_content} =
+                  SDL::Mixer::Music::load_MUS( $datum->{file} );
+            }
+            else {
+                $self->{data}->{$_}->{_content} =
+                  SDL::Mixer::Music::load_MUS($datum);
+            }
+        }
 
-	return 1;
+    }
+
+    return 1;
 }
 
 1;
