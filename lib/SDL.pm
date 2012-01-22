862ebeb283320b2126973df06851f849a0d29451
Tobias Leich <email@froggs.de>
2012-01-22 21:41:54 +0100

    dev release 2.537_01

--- lib/SDL.pm
+++ lib/SDL.pm
@@ -54,7 +54,7 @@ our %EXPORT_TAGS = (
 	defaults => $SDL::Constants::EXPORT_TAGS{'SDL/defaults'}
 );
 
-our $VERSION = '2.536';
+our $VERSION = '2.537_01';
 $VERSION = eval $VERSION;
 
 print "$VERSION" if ( defined( $ARGV[0] ) && ( $ARGV[0] eq '--SDLperl' ) );
