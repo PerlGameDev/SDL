70642cfe77d747791ee3e50493bf157116d846a5
Tobias Leich <email@froggs.de>
2012-06-12 23:07:33 +0200

    dev release 2.541_03, updated Alien::SDL version and changelog

--- lib/SDL.pm
+++ lib/SDL.pm
@@ -54,7 +54,7 @@ our %EXPORT_TAGS = (
 	defaults => $SDL::Constants::EXPORT_TAGS{'SDL/defaults'}
 );
 
-our $VERSION = '2.541_02';
+our $VERSION = '2.541_03';
 $VERSION = eval $VERSION;
 
 print "$VERSION" if ( defined( $ARGV[0] ) && ( $ARGV[0] eq '--SDLperl' ) );
